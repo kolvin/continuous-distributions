89847f7234444a24b839f3eb3faa6a10c6b0262d
github-action-benchmark <github@users.noreply.github.com>
2026-06-30 22:32:11 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 5ec18ab9de0e122ca7a6f005208c47b0fb67eb04

---
+++ benchmarks/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1782858731775,
+  "repoUrl": "https://github.com/kolvin/continuous-distributions",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "15124052+kolvin@users.noreply.github.com",
+            "name": "Calvin Taylor",
+            "username": "kolvin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5ec18ab9de0e122ca7a6f005208c47b0fb67eb04",
+          "message": "fix: reorder plugins so exec updates csproj before git commits it (#16)",
+          "timestamp": "2026-06-30T19:30:43-03:00",
+          "tree_id": "8cb9f16fd63eb73f818843ba5b7a762911124902",
+          "url": "https://github.com/kolvin/continuous-distributions/commit/5ec18ab9de0e122ca7a6f005208c47b0fb67eb04"
+        },
+        "date": 1782858731541,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Distributions.Benchmarks.PdfBenchmarks.NormalPdf",
+            "value": 25.181742290655773,
+            "unit": "ns",
+            "range": "± 0.021367847794404267"
+          },
+          {
+            "name": "Distributions.Benchmarks.PdfBenchmarks.LogNormalPdf",
+            "value": 24.560088074633054,
+            "unit": "ns",
+            "range": "± 0.03290832715711387"
+          }
+        ]
+      }
+    ]
+  }
+}
